022a977fa4f68e3f2724b73bb6de216504b005f9
Vineel Sai <mail@vineelsai.com>
2022-12-27 16:31:55 +0530

    rename project to FedoraWSL

--- build.bat
+++ build.bat
@@ -35,11 +35,11 @@ shift
 goto :ARGS_LOOP
 
 :POST_ARGS_LOOP
-%MSBUILD% %~dp0\DistroLauncher.sln /t:%_MSBUILD_TARGET% /m /nr:true /p:Configuration=%_MSBUILD_CONFIG%;Platform=x64
+%MSBUILD% %~dp0\FedoraWSL.sln /t:%_MSBUILD_TARGET% /m /nr:true /p:Configuration=%_MSBUILD_CONFIG%;Platform=x64
 
 if (%ERRORLEVEL%) == (0) (
     echo.
-    echo Created appx in %~dp0x64\%_MSBUILD_CONFIG%\DistroLauncher-Appx\
+    echo Created appx in %~dp0x64\%_MSBUILD_CONFIG%\FedoraWSL-Appx\
     echo.
 )
 
